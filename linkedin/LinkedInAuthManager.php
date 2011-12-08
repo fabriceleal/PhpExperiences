71c35b3413b9d1ad6eebf98b6abb6168729b1a54
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-08 16:29:15 +0000

    really working, begin with connections ...

--- linkedin/LinkedInAuthManager.php
+++ linkedin/LinkedInAuthManager.php
@@ -12,26 +12,58 @@ class LinkedInAuthManager extends ContainerFramework{
 		// Begin of javascript code
 		$javascript=<<<EOD
 
-	function onLoadLI(){
-		document.getElementById('results').innerHTML = "<p>called onLoad</p>";
+	function onLoadLI(){		
 		onLoadPutMeOnDiv('results');		
 	}
 	
 	function onLoadPutMeOnDiv(div){
-		document.getElementById('results').innerHTML = "<p>called onLoadPutMeOnDiv</p>";
-		IN.Event.on(IN, "auth", onAuth);
+		// On Load
+		document.getElementById(div).innerHTML = "<p>called onLoadPutMeOnDiv</p>";
+		IN.Event.on(IN, "auth", function (){
+			// On Auth
+			document.getElementById(div).innerHTML = "<p>called onAuth</p>";
+			
+			// Retrieve profile data
+			IN.API.Profile("me")
+				.fields("firstName", "lastName", "industry")
+				.result(function (profiles){
+					// On Load
+					document.getElementById(div).innerHTML = "";	
+					var members = profiles.values;
+					for (var member in members) {
+						document.getElementById('results').innerHTML += 
+								"<p> :D Hello " + 
+								members[member].firstName + " " + 
+								members[member].lastName + "</p>";
+					}
+				})
+				.error(function(err){
+					// On error
+					// notify user and log error
+					document.getElementById(div).innerHTML = "<p>Oops! on profile ...</p>";
+  					console.log(error);
+				});
+			
+			// Retrieve connections
+			IN.API.Connections("me")
+			    .fields("firstName", "lastName", "industry")
+    			.result(function (connections){
+    				// on Connections
+    				
+    			})
+    			.error(function(err){
+					// On error
+					// notify user and log error
+					document.getElementById(div).innerHTML = "<p>Oops! on connections ...</p>";
+  					console.log(error);
+				});
+			
+ 		});
 	}
 	
-	function onAuth(){
-		document.getElementById('results').innerHTML = "<p>called onAuth</p>";
-		IN.API.Profile("me").result(onResult);
- 	}
+	
  	
- 	function onResult(profiles){
-		document.getElementById('results').innerHTML = "<p>called onResult</p>";
-		member = profiles.values[0];
-		document.getElementById('results').innerHTML = "<p id=\"" + member.id + "\"> :D Hello " +  member.firstName + " " + member.lastName + "</p>";
-	}
+
 
 EOD;
 		// End of javascript code
