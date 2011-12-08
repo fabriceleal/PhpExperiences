e7e4c870602bc41d6898a697a194ac3f73cfae31
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-08 16:19:41 +0000

    restore

---
+++ linkedin/LinkedInApi.php
@@ -0,0 +1,15 @@
+<?php
+
+class LinkedInApi{
+	private static $key;
+		
+	static function getKey(){
+		if(!self::$key){
+			$config = parse_ini_file('privateLinkedIn.php');
+			self::$key = $config['key'];
+		}
+		return self::$key;
+	}
+}
+
+?>
