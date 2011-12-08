b4a0565ee22d2f1bf9be870c4eb9234b3749ac55
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-08 15:32:11 +0000

    try #2

--- index.php
+++ index.php
@@ -7,16 +7,15 @@ $page = new HtmlFramework();
 
 $page->getHead()
 		->setTitle('Fab Framework Usage')
-		->addHtmlObject(new LinkedInAuthManager())
-		->addHtmlObject(new LinkedInFramework(true, LinkedInAuthManager::ONLOAD));
-
+		->addHtmlObject(new LinkedInFramework(true, LinkedInAuthManager::ONLOAD))
+		->addHtmlObject(new LinkedInAuthManager()); //
 
 $page->getBody()
 		->addBruteText('Olá isto é texto limpo, antes do paragrafo')
 		->addLoneTag('p')
 		->addHtmlObject(new LinkedInSignIn())
 		->addBruteText('Olá, isto é texto limpo depois do paragrafo')
-		->addHtmlObject(new DivTag('id'));
+		->addHtmlObject(new DivTag(LinkedInAuthManager::DIV));
 
 
 ?>
